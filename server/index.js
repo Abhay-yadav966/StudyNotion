7844e77693d207003255f08ec1674961857df28d
Abhay Yadav <yadavabhay966@gmail.com>
2024-03-13 22:45:21 +0530

    origin change

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"https://studynotion-abhay.vercel.app",
         credentials:true,
     })
 );
